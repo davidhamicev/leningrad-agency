8f47b814f7c4b85a3160a2a7f0cb8b9d70cc15d9
David Hamicev <davidhamicev@mail.ru>
2020-06-13 12:09:50 +0300

    footer position

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,11 +5,13 @@ import Header from '../header';
 import Footer from '../footer';
 import AboutPage from '../pages/about-page';
 
+import './app.scss';
+
 const App = () => {
 	return (
-		<main className="main">
+		<main className="app">
 			<Header />
-			<div className="container">
+			<div className="container app__content">
 				<Switch>
 					<Route path="/" component={AboutPage} exact/>
 					<Route path="/services" render={() => <h2>Services page</h2>}/>
